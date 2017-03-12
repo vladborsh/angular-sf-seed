c8c8a93bc7173ed145ff62d85fd31143bf39fa49
vladborssh <vladborsh95s322@gmail.com>
2017-03-12 18:17:32 +0200

    configure gulp #2

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var zip = require('gulp-zip');
- 
+/* Run server */
 gulp.task('webserver', function() {
   gulp.src('app')
     .pipe(webserver({
@@ -14,20 +14,19 @@ gulp.task('webserver', function() {
       https: true
     }));
 });
-
- 
+/* Archire application */
 gulp.task('zip-app', function() {
   gulp.src('app/*')
     .pipe(zip('client-includes.zip'))
     .pipe(gulp.dest('build'))
   }
 );
-
+/* Archire vendor scripts application */
 gulp.task('zip-vendor', function() {
   gulp.src('b_components/*')
     .pipe(zip('vendor-includes.zip'))
     .pipe(gulp.dest('build'))
   }
 );
-
+/* Run server on default */
 gulp.task('default', [ 'webserver']);
