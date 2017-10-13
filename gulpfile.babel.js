8e4465815ac543b38ac96366a8ef7bd09c26f05f
Borsh Vlad <vlad.borsh@synebo.io>
2017-10-13 09:57:06 +0300

    Fix deploy env

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -56,11 +56,11 @@ gulp.task('deploy', () => {
     .then( () => utilSrv.zipNewPackage() )
     .then( () => { 
       if ( yargs.argv.username && yargs.argv.password && (yargs.argv.env || 'sb')) {
-        return deploySrv.deploy( yargs.argv.username, yargs.argv.password) 
+        return deploySrv.deploy( yargs.argv.username, yargs.argv.password, yargs.argv.env) 
       } else {
         const config = ((yargs.argv.env || 'sb') == 'sb') ? deployConfigSb : deployConfigProd;
         if ( config.username && config.password ) {
-          return deploySrv.deploy( config.username, config.password) 
+          return deploySrv.deploy( config.username, config.password, yargs.argv.env) 
         } else {
           return null;
         }
